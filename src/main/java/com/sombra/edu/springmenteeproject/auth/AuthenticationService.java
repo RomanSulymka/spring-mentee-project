d20522f8a56e384d023099d76e9d2d5e0594a6b6
Roman Sulymka <roma.sulimak@gmail.com>
2023-05-31 21:29:36 +0300

    fixed all comments

--- src/main/java/com/sombra/edu/springmenteeproject/auth/AuthenticationService.java
+++ src/main/java/com/sombra/edu/springmenteeproject/auth/AuthenticationService.java
@@ -91,20 +91,17 @@ public class AuthenticationService {
         tokenRepository.save(token);
     }
 
-    public void refreshToken(
-            HttpServletRequest request,
-            HttpServletResponse response
-    ) throws IOException {
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
         final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
         final String refreshToken;
         final String userEmail;
-        if (authHeader == null ||!authHeader.startsWith("Bearer ")) {
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             return;
         }
         refreshToken = authHeader.substring(7);
         userEmail = jwtService.extractUsername(refreshToken);
         if (userEmail != null) {
-            var user = this.repository.getUserAccountByEmail(userEmail) ;
+            var user = this.repository.getUserAccountByEmail(userEmail);
             if (jwtService.isTokenValid(refreshToken, user)) {
                 var accessToken = jwtService.generateToken(user);
                 revokeAllUserTokens(user);
@@ -113,7 +110,8 @@ public class AuthenticationService {
                         .accessToken(accessToken)
                         .refreshToken(refreshToken)
                         .build();
-                new ObjectMapper().writeValue(response.getOutputStream(), authResponse);
+                ObjectMapper objectMapper = new ObjectMapper();
+                objectMapper.writeValue(response.getOutputStream(), authResponse);
             }
         }
     }
